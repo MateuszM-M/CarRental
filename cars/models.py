dcf7aa9bcb8bc7c4a8c7dfa334b06ab8ade73ce1
MateuszM-M <mateuszmikulski2@gmail.com>
2021-07-05 19:28:41 +0200

    added cars database

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    CONDITION = (
+        ('used', 'used'),
+        ('new', 'new'),
+    )
+    
+    brand = models.CharField(max_length=40)
+    model = models.CharField(max_length=50)
+    engine = models.CharField(max_length=50)
+    year = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    condition = models.CharField(max_length=50, choices=CONDITION)
+    day_price = models.DecimalField(max_digits=6, decimal_places=2)
+    hour_price = models.DecimalField(max_digits=5, decimal_places=2)
+    
+    def __str__(self):
+        return '{} {}'.format(self.brand, self.model)
+    
+class CarPhoto(models.Model):
+    car = models.ForeignKey(Car, 
+                            related_name='photos', 
+                            on_delete=models.CASCADE)
+    photo = models.ImageField(blank=True, null=True, upload_to='photos')
