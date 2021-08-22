3bdb7fff1c11dd0b4edbca225e38f0278b56bd49
MateuszM-M <mateuszmikulski2@gmail.com>
2021-08-22 19:28:59 +0200

    added car photos on car list

--- cars/serializers.py
+++ cars/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
-from .models import Car
+from .models import Car, CarPhoto
+
+
+class CarPhotoSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = CarPhoto
+        fields = ['photo']
+
 
 class CarSerializer(serializers.HyperlinkedModelSerializer):
+    photos = CarPhotoSerializer(many=True, read_only=True)
+    
+    
     class Meta:
         model = Car
-        fields = ['brand', 'model', 'engine', 'year', 'location', 
-                  'condition', 'day_price', 'hour_price']
+        fields = ['url', 'brand', 'model', 'engine', 'year', 'location', 
+                  'condition', 'day_price', 'hour_price', 'photos']
+        
--- cars/urls.py
+++ cars/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from snippets import views
+from . import views
 
 
 router = DefaultRouter()
 router.register(r'cars', views.CarViewSet)
+
+
 
 urlpatterns = [
     path('', include(router.urls)),
