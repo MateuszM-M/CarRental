f5c51fcf56a5aaf0d67e514f405d7180caefe00d
MateuszM-M <mateuszmikulski2@gmail.com>
2021-08-29 14:50:57 +0200

    added booking model

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cars.models import Car
 
-# Create your models here.
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    booking_start = models.DateField()
+    booking_end = models.DateField()
