e29e51e0778f852623afb67bb47aebe033dea877
MateuszM-M <mateuszmikulski2@gmail.com>
2021-08-29 15:49:08 +0200

    view bookings by staff

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .serializers import BookingSerializer
+from rest_framework import viewsets
+from .models import Booking
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+
+class BookingViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing booking instances.
+    """
+    serializer_class = BookingSerializer
+    queryset = Booking.objects.all()
+    permission_classes = [IsAdminUser]
