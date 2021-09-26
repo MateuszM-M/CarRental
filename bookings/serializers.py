bd421b24d843ec2ef7f33864ad92f03872577704
MateuszM-M <mateuszmikulski2@gmail.com>
2021-09-26 11:53:36 +0200

    added car details to booking details

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import Booking
+from cars.serializers import CarSerializer
 
 class BookingSerializer(serializers.ModelSerializer):
+    car = CarSerializer(read_only=True)
+    
     class Meta:
         model = Booking
         fields = ['user', 'car', 'booking_start', 'booking_end']
