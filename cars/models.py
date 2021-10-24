0a203059b26d26a3833f7d56e9d32f01acaa49f1
MateuszM-M <mateuszmikulski2@gmail.com>
2021-10-24 23:11:45 +0200

    prod photo upload fix

--- cars/models.py
+++ cars/models.py
@@ -1,13 +1,16 @@
+from io import BytesIO
+
+from django.core.files.base import ContentFile
 from django.db import models
 from PIL import Image
 
 
 class Car(models.Model):
     CONDITION = (
-        ('used', 'used'),
-        ('new', 'new'),
+        ("used", "used"),
+        ("new", "new"),
     )
-    
+
     brand = models.CharField(max_length=40)
     model = models.CharField(max_length=50)
     engine = models.CharField(max_length=50)
@@ -16,22 +19,24 @@ class Car(models.Model):
     location = models.CharField(max_length=50)
     condition = models.CharField(max_length=50, choices=CONDITION)
     day_price = models.DecimalField(max_digits=6, decimal_places=2)
-    
+
     def __str__(self):
-        return '{} {}'.format(self.brand, self.model)
-    
-    
+        return "{} {}".format(self.brand, self.model)
+
+
 class CarPhoto(models.Model):
-    car = models.ForeignKey(Car, 
-                            related_name='photos', 
-                            on_delete=models.CASCADE)
-    photo = models.ImageField(blank=True, null=True, upload_to='photos')
-    
+    car = models.ForeignKey(Car, related_name="photos", on_delete=models.CASCADE)
+    photo = models.ImageField(blank=True, null=True, upload_to="photos")
+
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        img = Image.open(self.photo.path)
-        
-        if img.height > 800 or img.width > 800:
+        image = Image.open(self.photo)
+
+        if image.height > 800 or image.width > 800:
             output_size = (800, 800)
-            img.thumbnail(output_size)
-            img.save(self.photo.path)
+            imageBuffer = BytesIO()
+            image.thumbnail(output_size, Image.ANTIALIAS)
+            image.save(imageBuffer, image.format)
+
+
+            
